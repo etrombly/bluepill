c386e84f5a9bc96ed0b13eb4f3b39dd27b0b3565
Eric Trombly <etrombly@yahoo.com>
2017-05-18 18:04:43 +0200

    pin init now handles all pins

--- src/pin.rs
+++ src/pin.rs
@@ -26,16 +26,50 @@ impl<'a> Pin<'a>{
         }
 
         // Configure pin 13 as output
-        // need to allow other pins to be configured and be able to change pin mode
-        self
-            .port
-            .crh
-            .modify(
-                |_, w| {
-                    w.mode13()
-                        .output()
+        // still need to set cnf bits and handle analog/digital
+
+        match mode {
+            Mode::INPUT =>
+                match self.pin {
+                    0 => self.port.crl.modify(|_,w| w.mode0().input()),
+                    1 => self.port.crl.modify(|_,w| w.mode1().input()),
+                    2 => self.port.crl.modify(|_,w| w.mode2().input()),
+                    3 => self.port.crl.modify(|_,w| w.mode3().input()),
+                    4 => self.port.crl.modify(|_,w| w.mode4().input()),
+                    5 => self.port.crl.modify(|_,w| w.mode5().input()),
+                    6 => self.port.crl.modify(|_,w| w.mode6().input()),
+                    7 => self.port.crl.modify(|_,w| w.mode7().input()),
+                    8 => self.port.crh.modify(|_,w| w.mode8().input()),
+                    9 => self.port.crh.modify(|_,w| w.mode9().input()),
+                    10 => self.port.crh.modify(|_,w| w.mode10().input()),
+                    11 => self.port.crh.modify(|_,w| w.mode11().input()),
+                    12 => self.port.crh.modify(|_,w| w.mode12().input()),
+                    13 => self.port.crh.modify(|_,w| w.mode13().input()),
+                    14 => self.port.crh.modify(|_,w| w.mode14().input()),
+                    15 => self.port.crh.modify(|_,w| w.mode15().input()),
+                    _ => {},
                 },
-            );
+            Mode::OUTPUT =>
+                match self.pin {
+                    0 => self.port.crl.modify(|_,w| w.mode0().output()),
+                    1 => self.port.crl.modify(|_,w| w.mode1().output()),
+                    2 => self.port.crl.modify(|_,w| w.mode2().output()),
+                    3 => self.port.crl.modify(|_,w| w.mode3().output()),
+                    4 => self.port.crl.modify(|_,w| w.mode4().output()),
+                    5 => self.port.crl.modify(|_,w| w.mode5().output()),
+                    6 => self.port.crl.modify(|_,w| w.mode6().output()),
+                    7 => self.port.crl.modify(|_,w| w.mode7().output()),
+                    8 => self.port.crh.modify(|_,w| w.mode8().output()),
+                    9 => self.port.crh.modify(|_,w| w.mode9().output()),
+                    10 => self.port.crh.modify(|_,w| w.mode10().output()),
+                    11 => self.port.crh.modify(|_,w| w.mode11().output()),
+                    12 => self.port.crh.modify(|_,w| w.mode12().output()),
+                    13 => self.port.crh.modify(|_,w| w.mode13().output()),
+                    14 => self.port.crh.modify(|_,w| w.mode14().output()),
+                    15 => self.port.crh.modify(|_,w| w.mode15().output()),
+                    _ => {},
+                },
+        };
     }
 }
 
